726eb3f18dc3f71791f4dadf8614847fd8f04311
Roberto Carlos <betocarlos00@hotmail.com>
2021-12-29 22:57:01 -0300

    estudos js

---
+++ estudos js/codigos.js
@@ -0,0 +1,53 @@
+/*console.log("bem vindos ao stater")
+console.log(`${1+3}`)
+console.log(12.5 / 50)
+
+====================================
+
+variável global var
+
+console.log('x indefinido')
+{
+    var x = 4
+}
+console.log('existe x')
+
+====================================
+
+variável local let/ const
+
+console.log('y não definido')
+{
+    let y = 10
+    console.log('y definido')
+}
+console.log('y não definido')
+
+====================================
+
+arrays
+
+let animals = [
+            'lion', 'cat', 23, 'dog'
+            {
+                name: snake,
+                age: 2
+            }
+            ]
+
+console.log('animals[0]') == lion
+console.log('animals[4].name') == snake
+
+====================================
+
+estruturas
+
+const person = {
+    name: 'jhon',
+    age: 32,
+    weight: 88.6,
+    isAdmin: true
+}
+
+console.log(person.name)
+*/
---
+++ estudos js/scripts.js
@@ -0,0 +1,17 @@
+console.log('bem vindos ao stater')
+
+// let weight 
+// console.log(typeof weight)
+
+let user = {
+    name: 'beto',
+    age: 25,
+    weight: 88.6,
+    stars: 5.7,
+    isSubscribed: true
+}
+
+// let student = {};
+// console.log(typeof student)
+
+// console.log(user.name+" de idade "+ user.age +" pesa "+ user.weight)
